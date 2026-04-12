912bfd9a2e1bc676faaa327dee6d60b55bf2f2ec
le10x <leanderdrb@gmail.com>
2026-04-12 01:07:03 -0400

    Refactor settings popup to settings layer

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,101 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
 
-// --- CLASE PARA EL MENÚ AZUL PERSONALIZADO ---
-class MySettingsPopup : public geode::Popup<CCObject*> {
-protected:
-    bool setup(CCObject* obj) override {
-        this->setTitle("Level Functions");
-
-        auto menu = CCMenu::create();
-        menu->setLayout(ColumnLayout::create()->setGap(10.f));
-        m_mainLayer->addChild(menu);
-
-        // Toggle: Hold Level
-        auto holdToggle = CCMenuItemToggler::createWithStandardSprites(
-            this, menu_selector(MySettingsPopup::onToggle), 0.8f
-        );
-        holdToggle->setTag(1);
-        holdToggle->toggle(Mod::get()->getSettingValue<bool>("hold-level"));
-
-        auto holdLabel = CCLabelBMFont::create("Hold Level", "bigFont.fnt");
-        holdLabel->setScale(0.5f);
-
-        // Toggle: Ignore Clicks
-        auto ignoreToggle = CCMenuItemToggler::createWithStandardSprites(
-            this, menu_selector(MySettingsPopup::onToggle), 0.8f
-        );
-        ignoreToggle->setTag(2);
-        ignoreToggle->toggle(Mod::get()->getSettingValue<bool>("ignore-clicks"));
-
-        auto ignoreLabel = CCLabelBMFont::create("Ignore Clicks", "bigFont.fnt");
-        ignoreLabel->setScale(0.5f);
-
-        // Crear filas
-        auto row1 = CCMenu::create(holdToggle, holdLabel, nullptr);
-        row1->setContentSize({200, 30});
-        row1->setLayout(RowLayout::create()->setGap(10.f));
-        
-        auto row2 = CCMenu::create(ignoreToggle, ignoreLabel, nullptr);
-        row2->setContentSize({200, 30});
-        row2->setLayout(RowLayout::create()->setGap(10.f));
-
-        menu->addChild(row1);
-        menu->addChild(row2);
-        menu->updateLayout();
-
-        return true;
-    }
-
-    void onToggle(CCObject* sender) {
-        auto toggle = static_cast<CCMenuItemToggler*>(sender);
-        // En Geode, isToggled() devuelve el estado visual ANTES del cambio
-        bool newState = !toggle->isToggled(); 
-        
-        if (toggle->getTag() == 1) {
-            Mod::get()->setSettingValue("hold-level", newState);
-            if (auto pl = PlayLayer::get()) {
-                if (newState) pl->m_player1->pushButton(PlayerButton::Jump);
-                else pl->m_player1->releaseButton(PlayerButton::Jump);
-            }
-        } else {
-            Mod::get()->setSettingValue("ignore-clicks", newState);
-        }
-    }
-
+// --- MENÚ AZUL (FLAlertLayer) ---
+class MySettingsLayer : public FLAlertLayer {
 public:
-    static MySettingsPopup* create() {
-        auto ret = new MySettingsPopup();
-        if (ret && ret->init(220, 150, nullptr, "GJ_square01.png")) {
+    static MySettingsLayer* create() {
+        auto ret = new MySettingsLayer();
+        if (ret && ret->init(150)) { // 150 es la opacidad del fondo
             ret->autorelease();
             return ret;
         }
         CC_SAFE_DELETE(ret);
         return nullptr;
     }
+
+    bool init(int opacity) {
+        if (!FLAlertLayer::init(opacity)) return false;
+
+        auto winSize = CCDirector::get()->getWinSize();
+        
+        // Fondo azul
+        auto bg = CCScale9Sprite::create("GJ_square01.png");
+        bg->setContentSize({220, 150});
+        bg->setPosition(winSize / 2);
+        m_mainLayer->addChild(bg);
+
+        auto menu = CCMenu::create();
+        m_mainLayer->addChild(menu);
+
+        // Título
+        auto title = CCLabelBMFont::create("Level Functions", "goldFont.fnt");
+        title->setPosition({winSize.width / 2, winSize.height / 2 + 55});
+        title->setScale(0.7f);
+        m_mainLayer->addChild(title);
+
+        // Botón de cerrar
+        auto closeSprite = CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png");
+        auto closeBtn = CCMenuItemSpriteExtra::create(closeSprite, this, menu_selector(MySettingsLayer::onClose));
+        closeBtn->setPosition({-100, 65});
+        menu->addChild(closeBtn);
+
+        // --- TOGGLES ---
+        addToggle(menu, "Hold Level", "hold-level", {0, 20});
+        addToggle(menu, "Ignore Clicks", "ignore-clicks", {0, -20});
+
+        return true;
+    }
+
+    void addToggle(CCMenu* menu, const char* text, const char* settingKey, CCPoint pos) {
+        auto label = CCLabelBMFont::create(text, "bigFont.fnt");
+        label->setScale(0.4f);
+        label->setPosition({pos.x + 20, pos.y});
+        label->setAnchorPoint({0, 0.5});
+        menu->addChild(label);
+
+        auto toggle = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(MySettingsLayer::onToggle), 0.7f);
+        toggle->setPosition({pos.x - 40, pos.y});
+        toggle->setTag(settingKey[0]); // Usamos la primera letra como ID rápido
+        toggle->toggle(Mod::get()->getSettingValue<bool>(settingKey));
+        menu->addChild(toggle);
+    }
+
+    void onToggle(CCObject* sender) {
+        auto toggle = static_cast<CCMenuItemToggler*>(sender);
+        bool newState = !toggle->isToggled();
+        const char* key = (toggle->getTag() == 'h') ? "hold-level" : "ignore-clicks";
+        
+        Mod::get()->setSettingValue(key, newState);
+
+        if (auto pl = PlayLayer::get()) {
+            if (toggle->getTag() == 'h') {
+                if (newState) pl->m_player1->pushButton(PlayerButton::Jump);
+                else pl->m_player1->releaseButton(PlayerButton::Jump);
+            }
+        }
+    }
+
+    void onClose(CCObject*) {
+        this->setKeypadEnabled(false);
+        this->removeFromParentAndCleanup(true);
+    }
 };
 
-// --- LOGICA DEL JUEGO ---
+// --- RESTO DEL CÓDIGO (PlayLayer y PauseLayer) ---
 class $modify(MyPlayLayer, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontSave) {
         if (!PlayLayer::init(level, useReplay, dontSave)) return false;
-        
-        if (Mod::get()->getSettingValue<bool>("hold-level")) {
-            this->m_player1->pushButton(PlayerButton::Jump);
-        }
+        if (Mod::get()->getSettingValue<bool>("hold-level")) m_player1->pushButton(PlayerButton::Jump);
         return true;
     }
 
     void handleButton(bool hold, int button, bool isPlayer2) {
         if (Mod::get()->getSettingValue<bool>("ignore-clicks")) return;
-
-        if (hold && Mod::get()->getSettingValue<bool>("hold-level")) {
-            Mod::get()->setSettingValue("hold-level", false);
-        }
+        if (hold && Mod::get()->getSettingValue<bool>("hold-level")) Mod::get()->setSettingValue("hold-level", false);
         PlayLayer::handleButton(hold, button, isPlayer2);
     }
 
@@ -108,7 +108,6 @@ class $modify(MyPlayLayer, PlayLayer) {
     }
 };
 
-// --- BOTÓN DE PAUSA ---
 class $modify(MyPauseLayer, PauseLayer) {
     void customSetup() {
         PauseLayer::customSetup();
@@ -116,11 +115,8 @@ class $modify(MyPauseLayer, PauseLayer) {
         auto side = Mod::get()->getSettingValue<std::string>("button-side");
 
         auto btnSprite = CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png");
-        btnSprite->setScale(0.5f); 
-
-        auto btn = CCMenuItemSpriteExtra::create(
-            btnSprite, this, menu_selector(MyPauseLayer::onMySettings)
-        );
+        btnSprite->setScale(0.5f);
+        auto btn = CCMenuItemSpriteExtra::create(btnSprite, this, menu_selector(MyPauseLayer::onMySettings));
 
         auto menu = CCMenu::create();
         float posX = (side == "left") ? 25.f : winSize.width - 25.f;
@@ -130,6 +126,7 @@ class $modify(MyPauseLayer, PauseLayer) {
     }
 
     void onMySettings(CCObject*) {
-        MySettingsPopup::create()->show();
+        auto popup = MySettingsLayer::create();
+        CCDirector::get()->getRunningScene()->addChild(popup, 100);
     }
 };
