bd90a03287ce45be0df2ba41d12105a9611ace0c
le10x <leanderdrb@gmail.com>
2026-04-13 11:55:12 -0400

    Add MirrorPopup for mirror mode settings
    Implement MirrorPopup class for level settings with mirror mode toggle.

---
+++ src/MirrorPopup.cpp
@@ -0,0 +1,43 @@
+#include "MirrorPopup.hpp"
+
+MirrorPopup* MirrorPopup::create() {
+    auto ret = new MirrorPopup();
+    if (ret && ret->initAnchored(220.f, 150.f)) {
+        ret->autorelease();
+        return ret;
+    }
+    CC_SAFE_DELETE(ret);
+    return nullptr;
+}
+
+bool MirrorPopup::setup() {
+    this->setTitle("Level Settings");
+
+    auto pl = PlayLayer::get();
+    bool isMirror = pl ? pl->m_levelSettings->m_mirrorMode : false;
+
+    auto toggler = CCMenuItemToggler::createWithStandardSprites(
+        this, menu_selector(MirrorPopup::onMirrorToggle), 0.8f
+    );
+    toggler->toggle(isMirror);
+
+    auto label = CCLabelBMFont::create("Mirror Mode", "bigFont.fnt");
+    label->setScale(0.5f);
+
+    auto menu = CCMenu::create();
+    menu->addChild(toggler);
+    menu->addChild(label);
+    menu->setLayout(RowLayout::create()->setGap(10.f));
+    menu->setPosition({110.f, 70.f});
+    
+    m_mainLayer->addChild(menu);
+    return true;
+}
+
+void MirrorPopup::onMirrorToggle(CCObject* sender) {
+    if (auto pl = PlayLayer::get()) {
+        bool enabled = !static_cast<CCMenuItemToggler*>(sender)->isToggled();
+        pl->m_levelSettings->m_mirrorMode = enabled;
+        pl->toggleMirrorMode(enabled);
+    }
+}
