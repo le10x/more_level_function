8a212215c5bc9ec9c13dc5f6f11ca1a09c699e7c
le10x <leanderdrb@gmail.com>
2026-04-13 17:21:34 -0400

    Refactor MirrorPopup to extend FLAlertLayer
    Updated MirrorPopup class to inherit from FLAlertLayer and modified method signatures.

--- src/MirrorPopup.hpp
+++ src/MirrorPopup.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include <Geode/Geode.hpp>
 
-class MirrorPopup : public geode::Popup<std::string const&> {
+using namespace geode::prelude;
+
+class MirrorPopup : public FLAlertLayer {
 protected:
-    bool setup(std::string const& value) override;
-    void onMirrorToggle(cocos2d::CCObject* sender);
+    bool init() override;
+    void onMirrorToggle(CCObject* sender);
+    void onClose(CCObject* sender);
 
 public:
-    // Cambiamos el create para que coincida con el nuevo setup
     static MirrorPopup* create();
+    void show();
 };
