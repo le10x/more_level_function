a30fb8ab26072a4f9202cc547f8ed98edb599469
le10x <leanderdrb@gmail.com>
2026-04-13 17:11:49 -0400

    Modify mirror state handling in PlayLayer and PauseLayer

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@
 
 using namespace geode::prelude;
 
-// Variable global para el estado original
+// Variable estática para recordar el estado original del nivel
 static bool g_originalMirrorState = false;
 
 class $modify(MyPlayLayer, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontSave) {
         if (!PlayLayer::init(level, useReplay, dontSave)) return false;
+        
+        // Guardamos el estado original solo si los ajustes del nivel existen
         if (m_levelSettings) {
             g_originalMirrorState = m_levelSettings->m_mirrorMode;
         }
@@ -19,12 +21,15 @@ class $modify(MyPlayLayer, PlayLayer) {
 
     void resetLevel() {
         PlayLayer::resetLevel();
+        
+        // Si el usuario tiene activo "Reset on Death", devolvemos el Mirror a su estado original
         if (m_levelSettings && Mod::get()->getSettingValue<bool>("mirror-reset-on-death")) {
             m_levelSettings->m_mirrorMode = g_originalMirrorState;
         }
     }
 
     void onQuit() {
+        // Siempre restauramos al salir para no modificar el nivel permanentemente
         if (m_levelSettings) {
             m_levelSettings->m_mirrorMode = g_originalMirrorState;
         }
@@ -36,15 +41,21 @@ class $modify(MyPauseLayer, PauseLayer) {
     void customSetup() {
         PauseLayer::customSetup();
 
+        // Creamos el sprite del engranaje original de GD
         auto gearSprite = CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png");
         gearSprite->setScale(0.7f);
 
         auto gearBtn = CCMenuItemSpriteExtra::create(
-            gearSprite, this, menu_selector(MyPauseLayer::onOpenMirrorMenu)
+            gearSprite, 
+            this, 
+            menu_selector(MyPauseLayer::onOpenMirrorMenu)
         );
 
+        // Creamos un menú para contener el botón
         auto menu = CCMenu::create();
         menu->addChild(gearBtn);
+        
+        // Posición a la izquierda (ajusta estos valores si se encima con otros botones)
         menu->setPosition({35, 35});
         menu->setID("le10x-settings-menu"_spr);
 
@@ -52,6 +63,7 @@ class $modify(MyPauseLayer, PauseLayer) {
     }
 
     void onOpenMirrorMenu(CCObject* sender) {
+        // Llamamos al método create() que ahora maneja el template correctamente
         if (auto popup = MirrorPopup::create()) {
             popup->show();
         }
