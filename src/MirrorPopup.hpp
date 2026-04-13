cbfd10472936c35042132de53b4fe5918c55db33
le10x <leanderdrb@gmail.com>
2026-04-13 16:57:57 -0400

    Update MirrorPopup to use cocos2d::CCObject

--- src/MirrorPopup.hpp
+++ src/MirrorPopup.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
 
 using namespace geode::prelude;
 
-// Cambiado Geode::Popup por geode::Popup
 class MirrorPopup : public geode::Popup<> {
 protected:
     bool setup() override;
-    void onMirrorToggle(CCObject* sender);
+    void onMirrorToggle(cocos2d::CCObject* sender);
 public:
     static MirrorPopup* create();
 };
