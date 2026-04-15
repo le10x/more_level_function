4476fe0b281ed0f6738c379a5568d32a4680eb9b
le10x <leanderdrb@gmail.com>
2026-04-15 12:36:36 -0400

    Refactor MyMenuLayer class declaration

--- src/MyMenuLayer.hpp
+++ src/MyMenuLayer.hpp
@@ -1,11 +1,12 @@
+#pragma once
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
 
 using namespace geode::prelude;
 
-// Heredamos de Popup para tener el botón de cerrar automático
 class MyMenuLayer : public geode::Popup<> {
 protected:
-    bool setup() override; // Aquí se configura el contenido
+    bool setup() override; 
 public:
-    static MyMenuLayer* create(); // Método para instanciarla
+    static MyMenuLayer* create();
 };
